8d4159b4d1b70df06f5e800a8235febbd688d363
Abui <adjiem31@gmail.com>
2020-01-11 07:11:10 +0700

    Added build

---
+++ build/precache-manifest.b07ff1ebfc86f8d40ddbf793717aa8e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c476cebfb809f705e742e1718338fafa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7786bf1b8482d534392",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "947fb97d70cb8e03ae87",
+    "url": "/static/js/2.357aaadd.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.357aaadd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7786bf1b8482d534392",
+    "url": "/static/js/main.cb352a12.chunk.js"
+  },
+  {
+    "revision": "f3fafcd8ac14c72b0ad5",
+    "url": "/static/js/runtime-main.8f6f6e27.js"
+  },
+  {
+    "revision": "8fad69fc6dbcf7f7596b5c3d9781f432",
+    "url": "/static/media/bg.8fad69fc.png"
+  },
+  {
+    "revision": "5a6343d56f579f7c3bdce9419f3b1eb4",
+    "url": "/static/media/loading.5a6343d5.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6f2bd0bd9670ec60a57548e6229b58b9",
+    "url": "/static/media/logo.6f2bd0bd.png"
+  }
+]);
